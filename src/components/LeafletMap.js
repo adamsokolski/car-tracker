7077442b9620484aa4d5027f9fbc90d95ad4303e
Adam Sokólski <idKrazu@gmail.com>
2022-01-22 22:25:55 +0100

    ✨ Add markers from api

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -16,6 +16,7 @@ export const LeafletMap = () => {
   const [error, setError] = useState(null)
   const [isLoaded, setIsLoaded] = useState(false)
   const [items, setItems] = useState([])
+  const [markers, setMarkers] = useState([])
   const position = [52.229, 21.011]
 
   const iconMarkup = renderToStaticMarkup(
@@ -31,13 +32,10 @@ export const LeafletMap = () => {
       .then((res) => res.json())
       .then(
         (result) => {
-          console.log(result)
+          setItems(result.objects)
           setIsLoaded(true)
-          setItems(result)
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
+
         (error) => {
           setIsLoaded(true)
           setError(error)
@@ -45,11 +43,34 @@ export const LeafletMap = () => {
       )
   }, [])
 
+  useEffect(() => {
+    if (isLoaded) {
+      console.log(items)
+      /*   for (const car of items) {
+        console.log(
+          Number(car.location.latitude.toFixed(4)),
+          Number(car.location.longitude.toFixed(4))
+        )
+      } */
+
+      const temp = items.map((car) => (
+        <Marker
+          key={car.id}
+          position={[car.location.latitude, car.location.longitude]}
+          icon={customMarkerIcon}
+        >
+          <Popup>{car.name}</Popup>
+        </Marker>
+      ))
+      setMarkers(temp)
+    }
+  }, [isLoaded])
+
   return (
     <Container>
       <MapContainer
         center={position}
-        zoom={13}
+        zoom={7}
         scrollWheelZoom={true}
         style={{ height: '100%', width: '100%' }}
       >
@@ -57,16 +78,8 @@ export const LeafletMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
         />
-        <Marker position={position} icon={customMarkerIcon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-        <Marker position={[52.229, 21.1]} icon={customMarkerIcon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+
+        {markers}
       </MapContainer>
     </Container>
   )
