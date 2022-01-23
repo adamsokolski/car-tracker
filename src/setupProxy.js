3aec3534b8fea693f1bdc8088486b63103b1cc6d
Adam Sokólski <idKrazu@gmail.com>
2022-01-23 19:11:42 +0100

    🚧 Add _redirects for Netlify

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
   app.use(
-    '/api-client-portal/map?objectType=VEHICLE',
+    '/api-client-portal/map?objectType=',
     createProxyMiddleware({
       target: 'https://dev.vozilla.pl',
       changeOrigin: true,
